53fc896559fa79292ed9e7babdeec1ab4494a612
pratikvishwaroop <139211423+pratikvishwaroop@users.noreply.github.com>
2023-12-17 13:30:00 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,5 @@
+num = [10,20,30]
+
+for x in num:
+    y=x*10
+    print(list(y))
---
+++ sample.py
@@ -0,0 +1,7 @@
+r=input("Enter the Radius of Circle :")
+r=int(r)
+def area(r):
+    area=3.142*r*r
+    print(area)
+    
+area(r)
